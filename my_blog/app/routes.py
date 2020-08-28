efd9ec4052b89585d2f8467a974b25e882fbf762
rootcanopy <dave.trees201@gmail.com>
2020-08-28 16:15:23 +0200

    still setting up havent started view or building yet just re-adjusting and becoming familiar with flask again

--- my_blog/app/routes.py
+++ my_blog/app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Lisa'}
+    posts = [ 
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Sweden!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
