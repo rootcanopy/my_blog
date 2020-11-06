20ae7e541ce186c97c53409971e16c18e3417c30
rootcanopy <dave.trees201@gmail.com>
2020-11-06 06:23:04 +0100

    need to reload the window so that debug doesnt run

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 import os
+import logging
 from flask import Flask
 from config import Config
+from logging.handlers import SMTPHandler
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -12,6 +14,24 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+
+
+# ERROR EMAIL BACKEND
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMIN'], subject='Blog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 
 
 from app import routes, models, errors
