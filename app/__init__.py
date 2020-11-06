f6df372ff0cd49a25fd99c4425a9679ae2845057
rootcanopy <dave.trees201@gmail.com>
2020-11-06 09:55:44 +0100

    added error logging to file instead of email, althouugh I still havent got the email log to send yet

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ import logging
 from flask import Flask
 from config import Config
 from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -32,6 +33,17 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+    # ERROR LOGS TO FILE
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('/logs/my_blog.log', maxBytes=10240,
+                                        backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(messages)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
 
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('My_Blog startup')
 
 from app import routes, models, errors
